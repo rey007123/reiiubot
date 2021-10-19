06fa73187f75d7cfea0e06b164b12831217668dc
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-20 00:10:17 +0700

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -3,14 +3,14 @@ from userbot import CMD_HELP
 from userbot.events import register
 
 
-@register(outgoing=True, pattern=r'^\.santet(?: |$)(.*)')
+@register(outgoing=True, pattern=r'^\.realsantet(?: |$)(.*)')
 async def typewriter(typew):
     typew.pattern_match.group(1)
-    await typew.edit("`Anda Telah Mengaktifkan Perintah Santet Online ツ`")
+    await typew.edit("`Mengaktifkan jampe jampe santet `")
     sleep(2)
-    await typew.edit("`Mencari Nama Orang Ini...`")
+    await typew.edit("`Mencari asal usul bocah ini...`")
     sleep(1)
-    await typew.edit("`Santet Online Segera Dilakukan`")
+    await typew.edit("`Santet akan dilakukan`")
     sleep(1)
     await typew.edit("0%")
     number = 1
@@ -36,27 +36,6 @@ async def typewriter(typew):
     number = number + 1
     sleep(0.03)
     await typew.edit(str(number) + "%   █▍")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▌")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▊")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █▉")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▎")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▍")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ██▌")
     number = number + 1
     sleep(0.03)
     await typew.edit(str(number) + "%   ██▊")
@@ -170,27 +149,6 @@ async def typewriter(typew):
     await typew.edit(str(number) + "%   ████████▊")
     number = number + 1
     sleep(0.03)
-    await typew.edit(str(number) + "%   ████████▉")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████▎")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████▍")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████▌")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████▊")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████▉")
-    number = number + 1
-    sleep(0.03)
     await typew.edit(str(number) + "%   ██████████")
     number = number + 1
     sleep(0.03)
@@ -233,29 +191,7 @@ async def typewriter(typew):
     await typew.edit(str(number) + "%   ████████████▎")
     number = number + 1
     sleep(0.03)
-    await typew.edit(str(number) + "%   ████████████▍")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████████████▌")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████████████▊")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   ████████████▉")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████████")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████████▎")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████████▍")
-    number = number + 1
-    sleep(0.03)
-    await typew.edit(str(number) + "%   █████████████▌")
-    number = number + 1
+    
     sleep(0.03)
     await typew.edit(str(number) + "%   █████████████▊")
     number = number + 1
@@ -313,7 +249,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Target Berhasil Tersantet dan akan mengalami kejang kejang awkwkwkwk`")
 
 
 CMD_HELP.update({

c3ee3516258bce77efcfaffa63a85b89b59651b8
rey007123 <66903937+rey007123@users.noreply.github.com>
2021-10-21 00:34:49 +0700

    Create Ccnfc.py

---
+++ Ccnfc.py
@@ -0,0 +1,13 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+#kecepatan mengetik
+        time.sleep(random.random() * 0.1)
+#ubah angka 0.1 sesuai keinginan kamu
+#angka terkecil adalah yang paling cepat
+#angka terbesar adalah yang paling lambat
+mengetik('hallo teman-teman w3wandroid\nselamat belajar dalam blog ilmu teknologi android\nterima kasih.')
